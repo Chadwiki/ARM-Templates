386e27b4bf35862ca5464e9681d1418a05952ae6
Chad Pryor <github@webtoed.com>
2015-08-03 12:57:04 -0700

    Fixes
    Fixed yml problems and cleaned up statements
    
    enabled marvel agent for testing.

--- elastic/elasticsearch-ubuntu-install.sh
+++ elastic/elasticsearch-ubuntu-install.sh
@@ -304,12 +304,14 @@ echo "discovery.zen.ping.multicast.enabled: false" >> /etc/elasticsearch/elastic
 echo "discovery.zen.ping.unicast.hosts: $HOSTS_CONFIG" >> /etc/elasticsearch/elasticsearch.yml
 
 # Configure for for Marvel/Monitoring cluster
-echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-echo "action.auto_create_index: true"  >> /etc/elasticsearch/elasticsearch.yml
 # Next needs cleaned up for use of LBIP (ext/int) add to json param
 echo "marvel.agent.exporter.es.hosts: [ $MARVEL_HOST ]" >> /etc/elasticsearch/elasticsearch.yml
+echo "marvel.agent.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+echo "action.auto_create_index: .marvel-*"  >> /etc/elasticsearch/elasticsearch.yml
+
 # Enable cross cors
 echo "http.cors.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+echo "bootstrap.mlockall: true" >> /etc/elasticsearch/elasticsearch.yml
 
 # Configure Elasticsearch node type
 log "Configure master/client/data node type flags mater-$MASTER_ONLY_NODE data-$DATA_NODE"
@@ -318,17 +320,17 @@ if [ ${MASTER_ONLY_NODE} -ne 0 ]; then
     log "Configure node as master only"
     echo "node.master: true" >> /etc/elasticsearch/elasticsearch.yml
     echo "node.data: false" >> /etc/elasticsearch/elasticsearch.yml
-    echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+    # echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
 elif [ ${DATA_NODE} -ne 0 ]; then
     log "Configure node as data only"
     echo "node.master: false" >> /etc/elasticsearch/elasticsearch.yml
     echo "node.data: true" >> /etc/elasticsearch/elasticsearch.yml
-    echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+    # echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
 elif [ ${CLIENT_ONLY_NODE} -ne 0 ]; then
     log "Configure node as data only"
     echo "node.master: false" >> /etc/elasticsearch/elasticsearch.yml
     echo "node.data: false" >> /etc/elasticsearch/elasticsearch.yml
-    echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+    # echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
 else
     log "Configure node for master and data"
     echo "node.master: true" >> /etc/elasticsearch/elasticsearch.yml
